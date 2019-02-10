4cb1f89092f3ef50d9c8c618a3c16c7baed3e6f1
iansinphi <32443446+iansinphi@users.noreply.github.com>
2019-02-09 22:07:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/productatlas/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.productatlas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+                                    MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchInventory(View view) {
+        Log.d(LOG_TAG, "Button clicked");
+        Intent intent = new Intent(this, Inventory.class);
+        startActivity(intent);
+    }
+}
