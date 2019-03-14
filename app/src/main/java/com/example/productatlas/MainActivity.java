69c7369d4a7afe4f5a01b5d00f4c4178511f6fa7
isaiahgregov <isaiahgregov@icloud.com>
2019-03-13 21:26:37 -0500

    I created the quick search, which lays over map for now. I'm just pushing now so everyone can not have any problems syncing / merging later. I'm going to move this quick search over to a new layout next. Actually, this was the original search that Jordan started, but I realized it actually suits the exact requirements for our quick search task, so, after we discuss, I believe this should be our quick search, and we can implement the basic search easily.

--- app/src/main/java/com/example/productatlas/MainActivity.java
+++ app/src/main/java/com/example/productatlas/MainActivity.java
@@ -12,14 +12,25 @@ import android.content.Intent;
 import android.widget.AdapterView;
 import android.widget.TextView;
 
+import android.graphics.Color;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
-        return true;
-    }
+    private static String[] SUGGESTION = new String[] {
+            "Apple", "Amazon", "Microsoft", "Alphabet", "Google", "Samsung", "Asus",
+            "HP", "Intel", "Qualcomn", "AMD", "Oracle", "Facebook", "Spotify"
+    };
+
+    private MaterialSearchView mMaterialSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +41,76 @@ public class MainActivity extends AppCompatActivity {
         //RYAN - initiates the database creation
         DBHandler dbHandler = new DBHandler(this);
         dbHandler.getWritableDatabase();
+
+        //The rest here (as of 3/13/19) creates the search bar. Isaiah
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        mMaterialSearchView = findViewById(R.id.searchView);
+//        mMaterialSearchView.setSuggestions(SUGGESTION);
+
+        final ListView listView = findViewById(R.id.listView);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SUGGESTION);
+        listView.setAdapter(arrayAdapter);
+
+        mMaterialSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, SUGGESTION);
+                listView.setAdapter(arrayAdapter);
+            }
+        });
+
+        mMaterialSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1);
+
+                if (newText != null && !newText.isEmpty()) {
+                    for (String s : SUGGESTION) {
+                        if (s.toLowerCase().contains(newText))
+                            arrayAdapter.add(s);
+                    }
+                } else {
+                    arrayAdapter.addAll(SUGGESTION);
+                }
+
+                listView.setAdapter(arrayAdapter);
+
+                return false;
+            }
+        });
     }
 
-    public void launchInventory(MenuItem menuItem) {
-        // intent signals when to start activity, Inventory. This is initiated by a Button press.
-        Log.d(LOG_TAG, "Button clicked");
-        Intent intent = new Intent(this, Inventory.class);
-        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.main_menu, menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.searchMenu);
+        mMaterialSearchView.setMenuItem(menuItem);
+        return super.onCreateOptionsMenu(menu);
     }
 
-    //I added this function but do not know how Jordan wants to implement. Isaiah
-    //This function uncommented crashes now, but will(?) bring up search feature after Menu class /
-    //      support_simple_spinner_dropdown_item.xml file / AndroidManifest.xml file is debugged /
-    //      is updated, to my current understanding. Isaiah.
-    public void launchSearch(MenuItem menuItem) {
+//    public void launchInventory(MenuItem menuItem) {
+//        // intent signals when to start activity, Inventory. This is initiated by a Button press.
 //        Log.d(LOG_TAG, "Button clicked");
-//        Intent intent = new Intent(this, selectionMenu.class);
+//        Intent intent = new Intent(this, Inventory.class);
 //        startActivity(intent);
-    }
+//    }
 
     //Jordan's function, but I do not know where to put. Isaiah
 //    public String getSearchString(){
