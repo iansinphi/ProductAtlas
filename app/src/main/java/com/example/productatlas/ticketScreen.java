fea9cd9b58754912a97b39f03e8053e0e92b5de4
jstennett <jstennett54@yahoo.com>
2019-04-10 11:34:45 -0500

    Committed by Jesse to add the information screen to github.

---
+++ app/src/main/java/com/example/productatlas/ticketScreen.java
@@ -0,0 +1,13 @@
+package com.example.productatlas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ticketScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_screen);
+    }
+}
